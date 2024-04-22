fb1e0fecc24db0f917af3ac2042960fc9039e437
Blake Johnson <blakeejohnson39@gmail.com>
2024-04-22 09:45:58 -0500

    feat: eth checksum address properly calculated

--- ape_aws_kms/accounts.py
+++ ape_aws_kms/accounts.py
@@ -1,3 +1,4 @@
+import asn1tools
 import boto3
 import ecdsa
 import json
@@ -19,6 +20,68 @@ from pydantic import BaseModel, Field
 from ape.api.accounts import AccountContainerAPI, AccountAPI, TransactionAPI
 from ape.types import AddressType, MessageSignature
 from ape.utils import cached_property
+
+from web3.auto import w3
+
+
+SECP256_K1_N = int("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
+
+
+def find_eth_signature(signature: bytes) -> dict:
+    signature_schema = asn1tools.compile_string(
+        '''
+        Signature DEFINITIONS ::= BEGIN
+
+        Ecdsa-Sig-Value  ::=  SEQUENCE  {
+               r     INTEGER,
+               s     INTEGER  }
+
+        END
+        '''
+    )
+
+    # https://tools.ietf.org/html/rfc3279#section-2.2.3
+    signature_decoded = signature_schema.decode(
+        'Ecdsa-Sig-Value', signature,
+    )
+    s = signature_decoded['s']
+    r = signature_decoded['r']
+    secp256_k1_n_half = SECP256_K1_N / 2
+    if s > secp256_k1_n_half:
+        s = SECP256_K1_N - s
+
+    return {'r': r, 's': s}
+
+
+def find_eth_address(public_key):
+    key = asn1tools.compile_string(
+        '''
+        Key DEFINITIONS ::= BEGIN
+
+        SubjectPublicKeyInfo  ::=  SEQUENCE  {
+           algorithm         AlgorithmIdentifier,
+           subjectPublicKey  BIT STRING
+         }
+
+        AlgorithmIdentifier  ::=  SEQUENCE  {
+            algorithm   OBJECT IDENTIFIER,
+            parameters  ANY DEFINED BY algorithm OPTIONAL
+          }
+
+        END
+        '''
+    )
+    key_decoded = key.decode('SubjectPublicKeyInfo', public_key)
+    pub_key_raw = key_decoded['subjectPublicKey'][0]
+    pub_key = pub_key_raw[1:len(pub_key_raw)]
+
+    # https://www.oreilly.com/library/view/mastering-ethereum/9781491971932/ch04.html
+    hex_address = w3.keccak(bytes(pub_key)).hex()
+    eth_address = '0x{}'.format(hex_address[-40:])
+    eth_checksum_addr = w3.to_checksum_address(eth_address)
+    breakpoint()
+    return eth_checksum_addr
+
 
 
 class AliasResponse(BaseModel):
@@ -56,7 +119,7 @@ class AwsAccountContainer(AccountContainerAPI):
     def accounts(self) -> Iterator[AccountAPI]:
         return map(
             lambda x: KmsAccount(
-                alias=x.alias,
+                key_alias=x.alias,
                 key_id=x.key_id,
                 key_arn=x.arn,
             ),
@@ -65,7 +128,7 @@ class AwsAccountContainer(AccountContainerAPI):
 
 
 class KmsAccount(AccountAPI):
-    alias: str
+    key_alias: str
     key_id: str
     key_arn: str
 
@@ -79,7 +142,7 @@ class KmsAccount(AccountAPI):
 
     @cached_property
     def address(self) -> AddressType:
-        return to_checksum_address(keccak(self.public_key)[:20])
+        return to_checksum_address(keccak(self.public_key[-64:])[-20:].hex().lower())
 
     def sign_raw_msghash(self, msghash: HexBytes) -> Optional[MessageSignature]:
         """
@@ -109,14 +172,15 @@ class KmsAccount(AccountAPI):
             v += 27
         v_byte = bytes([v])
         message_signature = MessageSignature.from_vrs(v_byte + r_bytes + s_bytes)
+        msg_sig = find_eth_signature(signature)
+        breakpoint()
         try:
             if self.check_signature(signable_message, message_signature):
-                return response
+                return message_signature
         except:
             pass
 
         raise ValueError("Signature failed to verify")
-        
 
     def sign_transaction(self, txn: TransactionAPI, **signer_options) -> Optional[TransactionAPI]:
         """
