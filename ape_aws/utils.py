6f8c33adae0af29d9104b13bcd0b7b217dee4a54
Blake Johnson <blakeejohnson39@gmail.com>
2024-04-30 16:09:52 -0500

    feat: remove unused code from utils

--- ape_aws/utils.py
+++ ape_aws/utils.py
@@ -1,33 +1,10 @@
 import ecdsa
 from datetime import datetime
-from eth_account.messages import encode_defunct
 
 from pydantic import BaseModel, Field
 
-from ape_ethereum.transactions import DynamicFeeTransaction
-
 
 SECP256_K1_N = int("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
-
-txn = {
-    "chain_id": 11155111,
-    "nonce": 0,
-    "value": 1,
-    "data": '0x00',
-    "receiver": "0xa5D3241A1591061F2a4bB69CA0215F66520E67cf",
-    "type": 2,
-    "max_fee": 100000000000,
-    "max_priority_fee": 300000000000,
-}
-
-transaction = DynamicFeeTransaction(**txn)
-
-
-def create_signable_message(msg):
-    """
-    To be removed, used for testing
-    """
-    return encode_defunct(text=msg)
 
 
 def _convert_der_to_rsv(
