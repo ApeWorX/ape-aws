0013ce6ce58f8678daeefed45021911b7a577fed
johnson2427 <blakeejohnson39@gmail.com>
2024-05-07 13:05:45 -0500

    feat: remove ape_cli_context from iam cli tool

--- ape_aws/iam/_cli.py
+++ ape_aws/iam/_cli.py
@@ -1,5 +1,4 @@
 import click
-from ape.cli import ape_cli_context
 
 from ape_aws.client import iam_client
 
@@ -10,12 +9,10 @@ def iam():
 
 
 @iam.command()
-@ape_cli_context()
-def list_admins(cli_ctx):
-    cli_ctx.logger.success(f"Administrators: {iam_client.list_admins()}")
+def list_admins():
+    click.echo(f"Administrators: {iam_client.list_admins()}")
 
 
 @iam.command()
-@ape_cli_context()
-def list_users(cli_ctx):
-    cli_ctx.logger.success(f"Users: {iam_client.list_users()}")
+def list_users():
+    click.echo(f"Users: {iam_client.list_users()}")
