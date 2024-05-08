db198536a7fa22e595a2f203df7027619d9276a1
johnson2427 <blakeejohnson39@gmail.com>
2024-05-08 07:56:05 -0500

    refactor: reorder imports

--- ape_aws/accounts.py
+++ ape_aws/accounts.py
@@ -6,8 +6,8 @@ from ape.types import AddressType, MessageSignature, SignableMessage, Transactio
 from eth_account._utils.legacy_transactions import serializable_unsigned_transaction_from_dict
 from eth_account.messages import _hash_eip191_message, encode_defunct
 from eth_pydantic_types import HexBytes
-from eth_utils import keccak, to_checksum_address
 from eth_typing import Hash32
+from eth_utils import keccak, to_checksum_address
 
 from .client import kms_client
 from .utils import _convert_der_to_rsv
