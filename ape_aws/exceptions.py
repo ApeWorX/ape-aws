e0804632c1322a591543909dd0b7d2bd78cc707c
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2025-05-12 11:37:32 -0400

    fix: avoid exceptions when accessing AWS account container (#26)

--- ape_aws/exceptions.py
+++ ape_aws/exceptions.py
@@ -1,5 +1,16 @@
+from typing import TYPE_CHECKING
+
 from ape.exceptions import ApeException
+from click.exceptions import UsageError
+
+if TYPE_CHECKING:
+    from botocore.exceptions import BotoCoreError  # type: ignore[import-untyped]
 
 
 class ApeAwsException(ApeException):
     pass  # NOTE: For subclassing
+
+
+class AwsAccessError(ApeAwsException, UsageError):
+    def __init__(self, exc: "BotoCoreError"):
+        super().__init__(f"[ape-aws] {exc}")
